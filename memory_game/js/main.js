60d2d74a174d912291557d6ef81d0e7232ca13ab
Christopher Albanese <631gti@gmail.com>
2019-11-28 10:19:08 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,21 @@
-var cardOne = 'queen';
-var cardTwo = 'king';
-var cardThree = 'king';
-var cardFour = 'queen';
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-console.log('User flipped ' + cardOne)
-console.log('User flipped ' + cardTwo)
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne); {
+	console.log('User flipped queen');
+}
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo); {
+	console.log('User flipped king');
+}
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert('You found a match!');
+}
+else {
+	alert('Sorry, try again.');
+}
